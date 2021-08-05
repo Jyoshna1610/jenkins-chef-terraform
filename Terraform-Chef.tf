e8d323e047130cc7d17c20f8f7b2dac404c8f404
Jyoshna1610 <65143653+Jyoshna1610@users.noreply.github.com>
2021-08-06 00:52:41 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web1" {
-   ami           = "ami-0c2b8ca1dad447f8a"
+   ami           = "ami-i-0ea2caded0c0eaddaa"
    instance_type = "t2.micro"
    count = 1
    vpc_security_group_ids = ["sg-0990c12803c100850"]
-   key_name               = "Linux_Terraform-Chef"
+   key_name               = "chef-terra.pem"
    
    
 provisioner "remote-exec" {
@@ -33,7 +33,7 @@ provisioner "remote-exec" {
     type     = "ssh"
     user     = "ec2-user"
     password = ""
-    private_key = file("${path.module}/Linux_Terraform-Chef.pem")
+    private_key = file("${path.module}/chef-terra.pem")
     host = self.public_ip
   }
   }
