3a1c84b33a73fc0cd50c1cfde52a60e6021c4f3a
Jyoshna1610 <65143653+Jyoshna1610@users.noreply.github.com>
2021-08-06 14:08:01 +0530

    Update Terraform-Chef.tf

--- Terraform-Chef.tf
+++ Terraform-Chef.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
    ami           = "ami-i-0ea2caded0c0eadda"
    instance_type = "t2.micro"
    count = 1
-   vpc_security_group_ids = [""]
+   vpc_security_group_ids = ["sg-073ed8e71e767cacb"]
    key_name               = "chef-terra.pem"
    
    
